afb6e7cc528994cdc95b3febf0c274f7a00b71da
Garv Nandwana <149328353+garvn10@users.noreply.github.com>
2024-10-08 21:49:27 +0530

    Add files via upload

---
+++ 7I.cpp
@@ -0,0 +1,20 @@
+// NAME - SHIVENDRA DWIVEDI
+// PRM - 23070123122
+// EXPERIMENT - 7(I) 
+
+#include<iostream>
+#include<string>
+using namespace std;
+int main() {
+string a;
+cout<<"Enter string: ";
+cin>>a;
+int i;
+for(i=a.length()-1;i>=0;i--) {
+cout<<a[i];
+}
+return 0;
+}
+
+// Enter string: Shloka 
+// akolhS 
