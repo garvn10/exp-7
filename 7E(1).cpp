734f37454af1f7c5fe025874c54806887a4f0040
Sumit Pandey <sumitpandey8569@gmail.com>
2025-09-04 11:27:35 +0530

    Update 7E(1).cpp

--- 7E(1).cpp
+++ 7E(1).cpp
@@ -1,6 +1,6 @@
 // NAME -GARV NANDWANA 
 // PRN - 23070123167
-// EXPERIMENT - 7 (E.1) 
+// EXPERIMENT - 7 (E7) 
 
 #include <iostream>
 using namespace std;
@@ -14,7 +14,7 @@ int main() {
   double count = 0.0;
   double average;
 
-  cout << "The numbers are: ";
+  cout << "The numbers are=";
 
   //  print array elements use of range-based for loop
   for (const double n : numbers) {
@@ -37,3 +37,4 @@ int main() {
 // Sum = 50.34
 //  
 // Their Average = 8.39      
+
